a06954b6d483763ca52210658593f53cb441c397
WonderXuy <veko.dmitriy@mail.ru>
2019-10-08 18:55:41 +0300

    feat: add task

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,84 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this._config = config;
+        this.currentState = this._config["initial"];
+        this.History = [];
+        this.arrRedo = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.currentState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        try{
+        if(state in this._config['states']){
+        for(let key in this._config['states']){
+            if(key==state){
+                this.History.push(this.currentState);
+                this.arrRedo.length = 0;
+                this.currentState = state;
+                break;
+            }
+        }
+    }
+    else{
+        throw new Error('is not existing transition of FSM');
+    }
+}
+finally{
+
+}
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        try{
+        for(let states in this._config["states"]){
+            if(states==this.currentState){
+            for(let transitions in this._config["states"][states]){
+                if(event in this._config["states"][states][transitions]){
+                for(let key in this._config["states"][states][transitions]){
+                    if(key==event){
+                        this.History.push(this.currentState);
+                        this.arrRedo.length = 0;
+                        this.currentState = this._config["states"][states][transitions][key];
+                        return;
+                    }
+                }
+            }
+            else{
+            throw new Error('is not existing event in corrent state of FSM');
+            }
+            }
+        }
+        }
+    }
+    finally{
+
+    }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.History.push(this.currentState);
+        this.currentState = this._config["initial"];
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +88,66 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event="") {
+        let result = [];
+        if(event == ""){
+            for(let key in this._config["states"]){
+                result.push(key);
+            }
+        }
+        else{
+            for(let states in this._config["states"]){
+                for(let transitions in this._config["states"][states]){
+                    for(let key in this._config["states"][states][transitions]){
+                        if(key==event){
+                            result.push(states);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this.History.length>0){
+           
+            this.arrRedo.push(this.currentState);
+            this.currentState = this.History.pop();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(this.arrRedo.length>0){
+            this.currentState = this.arrRedo.pop();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.History.length = 0;
+        this.arrRedo.length = 0;
+    }
 }
 
 module.exports = FSM;
